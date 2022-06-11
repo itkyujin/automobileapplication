c1326f4cc3ec67c4a8634f982d7f0dd4dcbfef65
itkyujin <104597854+itkyujin@users.noreply.github.com>
2022-06-10 23:26:51 -0400

    before basic layout with Angular Material

--- angular-material-mean-stack/src/app/app-routing.module.ts
+++ angular-material-mean-stack/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { Routes, RouterModule } from '@angular/router';
+import { AddCarComponent } from './components/add-car/add-car.component';
+import { EditCarComponent } from './components/edit-car/edit-car.component';
+import { CarsListComponent } from './components/cars-list/cars-list.component';
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'add-car' },
+  { path: 'add-car', component: AddCarComponent },
+  { path: 'edit-car/:id', component: EditCarComponent },
+  { path: 'cars-list', component: CarsListComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- angular-material-mean-stack/src/app/app.module.ts
+++ angular-material-mean-stack/src/app/app.module.ts
@@ -1,24 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCarComponent } from './components/add-car/add-car.component';
 import { EditCarComponent } from './components/edit-car/edit-car.component';
 import { CarsListComponent } from './components/cars-list/cars-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './material.module';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddCarComponent,
     EditCarComponent,
-    CarsListComponent
+    CarsListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
