495de68d28d0dcc9187b5e2fdb8b136a9e60198c
itkyujin <104597854+itkyujin@users.noreply.github.com>
2022-06-10 22:56:17 -0400

    added add-car component

--- angular-material-mean-stack/src/app/app.module.ts
+++ angular-material-mean-stack/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCarComponent } from './components/add-car/add-car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCarComponent
   ],
   imports: [
     BrowserModule,
