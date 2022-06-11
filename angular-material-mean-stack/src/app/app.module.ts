3c73907cb654c9647b7c0fd36261f927fe6cb636
itkyujin <104597854+itkyujin@users.noreply.github.com>
2022-06-10 23:00:22 -0400

    added edit-car component

--- angular-material-mean-stack/src/app/app.module.ts
+++ angular-material-mean-stack/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCarComponent } from './components/add-car/add-car.component';
+import { EditCarComponent } from './components/edit-car/edit-car.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddCarComponent
+    AddCarComponent,
+    EditCarComponent
   ],
   imports: [
     BrowserModule,
